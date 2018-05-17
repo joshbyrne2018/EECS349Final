9c46a190a6df2bddb3aa715b711ccabe7c96010c
cgarcia33 <cristobalgarcia2019@u.northwestern.edu>
2018-05-17 00:19:35 -0500

    fixed training and holdout data

--- split_data.py
+++ split_data.py
@@ -4,8 +4,8 @@ import random
 
 def split():
     fin = open("./merged_csv/combine2008-2018.csv", 'rb')
-    f_train_out = open("./training_and_holdout_data/combine_training_data", 'wb')
-    f_holdout_out = open("./training_and_holdout_data/combine_holdout_data", 'wb')
+    f_train_out = open("./training_and_holdout_data/combine_training_data.csv", 'wb')
+    f_holdout_out = open("./training_and_holdout_data/combine_holdout_data.csv", 'wb')
     f_holdout_out.write("School,Vertical,3Cone,Pos,Ht,Bench,Player,Broad Jump,Wt,College,40yd,Shuttle,Drafted (tm/rnd/yr)\n")
 
     nLines = sum(1 for line in fin)
