8935525aae4d7f40d070c7140136d449637e233b
drewhasse <drew091996@gmail.com>
2018-05-18 12:36:17 -0500

    binary drafted or not for combine training data

--- filter_round.py
+++ filter_round.py
@@ -30,3 +30,35 @@ def filter_round(infile):
         fout.writelines(line)
 
     fout.close()
+
+def drafted_or_not(infile,outfile):
+    lines = []
+    row = []
+    draft = []
+    newline = ""
+    f = open(infile, "r")
+    headers = f.readline()
+    ln = f.readline()
+    while ln:
+        newline = ""
+        row = ln.split(',')
+        draft = row[len(row)-1]
+        if '0' not in draft:
+            round = "1"
+        else:
+            round = draft
+        for i in range(0,len(row)-1):
+            newline += row[i] + ','
+        newline += round + '\n'
+        lines.append(newline)
+        ln = f.readline()
+
+    f.close()
+    fout = open(outfile, "w")
+    fout.write(headers)
+
+    for line in lines:
+        fout.writelines(line)
+
+    fout.close()
+drafted_or_not("combine_training_data.csv", "combine_training_binary.csv")
