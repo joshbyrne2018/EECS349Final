eb8203775a275c2dc7ca67c822770ac99b4098bd
cgarcia33 <cristobalgarcia2019@u.northwestern.edu>
2018-05-18 11:25:08 -0500

    Made holdout data more random

--- split_data.py
+++ split_data.py
@@ -4,17 +4,20 @@ import random
 
 def split():
     fin = open("./merged_csv/combine2008-2018.csv", 'rb')
+    data = fin.readlines()[1:]
+    random.shuffle(data)
+
     f_train_out = open("./training_and_holdout_data/combine_training_data.csv", 'wb')
     f_holdout_out = open("./training_and_holdout_data/combine_holdout_data.csv", 'wb')
+    f_train_out.write("School,Vertical,3Cone,Pos,Ht,Bench,Player,Broad Jump,Wt,College,40yd,Shuttle,Drafted (tm/rnd/yr)\n")
     f_holdout_out.write("School,Vertical,3Cone,Pos,Ht,Bench,Player,Broad Jump,Wt,College,40yd,Shuttle,Drafted (tm/rnd/yr)\n")
 
-    nLines = sum(1 for line in fin)
-    fin.seek(0)
-    nTrain = int(nLines * 0.75)
+    nLines = len(data)
+    nTrain = nLines * 0.75
     nHoldout = nLines - nTrain
 
     i = 0
-    for line in fin:
+    for line in data:
         r = random.random()
         if (i < nTrain and r < 0.75) or (nLines - i > nHoldout):
             f_train_out.write(line)
@@ -24,4 +27,4 @@ def split():
 
 
 if __name__ == '__main__':
-    split()
+    split() 
